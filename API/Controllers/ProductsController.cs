735d839326d7edc1fb0f57747bdabd6568f19b04
ttomczak <ttomczak1512@protonmail.com>
2023-05-22 23:58:59 -0500

    feat: repository pattern and migration config

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,7 +1,6 @@
 ﻿using Core.Entities;
-using Infrastructure.Data;
+using Core.Interfaces;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -9,23 +8,24 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        private readonly StoreContext _context;
+        private readonly IProductRepository _repo;
 
-        public ProductsController(StoreContext context)
+        public ProductsController(IProductRepository repo)
         {
-            _context = context;
+            _repo = repo;
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            return await _context.Products.ToListAsync();
+            var products = await _repo.GetProductsAsync();
+            return Ok(products);
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return await _context.Products.FindAsync(id);
+            return await _repo.GetProductByIdAysnc(id);
         }
     }
 }
---
+++ Core/Interfaces/IProductRepository.cs
@@ -0,0 +1,10 @@
+﻿using Core.Entities;
+
+namespace Core.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<Product> GetProductByIdAysnc(int id);
+        Task<IReadOnlyList<Product>> GetProductsAsync();
+    }
+}
